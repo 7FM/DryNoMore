ad5e8cb240c5286181f5869cfa15a852d1f56cc4
7FM <41307817+7FM@users.noreply.github.com>
2022-08-07 18:51:21 +0200

    extend status struct for convenience reasons

--- include/lan_protocol.hpp
+++ include/lan_protocol.hpp
@@ -8,7 +8,8 @@
 PACKED_STRUCT_DEF(Status,
                   uint8_t beforeMoistureLevels[MAX_MOISTURE_SENSOR_COUNT];
                   uint8_t afterMoistureLevels[MAX_MOISTURE_SENSOR_COUNT];
-                  uint8_t beforeWaterLevels[2]; uint8_t afterWaterLevels[2];);
+                  uint8_t beforeWaterLevels[2]; uint8_t afterWaterLevels[2];
+                  uint8_t numPlants; uint8_t numWaterSensors;);
 
 enum PacketType : uint8_t {
   INFO_MSG = 1,
