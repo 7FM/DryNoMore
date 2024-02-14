f64569d2810f7e2d8ee30c429b97012e25a0ccf4
7FM <41307817+7FM@users.noreply.github.com>
2024-02-14 22:05:46 +0100

    move location of shared headers

--- server/telegram_bot/include/settings_defs.hpp
+++ server/telegram_bot/include/settings_defs.hpp
@@ -1,1 +1,22 @@
-../../../include/settings_defs.hpp
+#pragma once
+
+#include "packed.hpp"
+
+#define MAX_MOISTURE_SENSOR_COUNT 6
+
+#define SETTINGS_VERSION_NUM 0x0011
+
+PACKED_STRUCT_DEF(SensConfig, uint16_t minValue; uint16_t maxValue;);
+PACKED_STRUCT_DEF(WaterLvlThresholds, uint8_t warnThres; uint8_t emptyThres;);
+
+PACKED_STRUCT_DEF(
+    Settings, SensConfig sensConfs[(MAX_MOISTURE_SENSOR_COUNT) + 2];
+    WaterLvlThresholds waterLvlThres[2];
+    uint8_t targetMoisture[MAX_MOISTURE_SENSOR_COUNT];
+    uint8_t burstDuration[MAX_MOISTURE_SENSOR_COUNT];
+    uint8_t burstDelay[MAX_MOISTURE_SENSOR_COUNT];
+    uint8_t maxBursts[MAX_MOISTURE_SENSOR_COUNT];
+    uint8_t ticksBetweenIrrigation[MAX_MOISTURE_SENSOR_COUNT]; uint8_t
+        moistSensToWaterSensBitmap[((MAX_MOISTURE_SENSOR_COUNT) + 8 - 1) / 8];
+    uint8_t skipBitmap[((MAX_MOISTURE_SENSOR_COUNT) + 8 - 1) / 8];
+    uint8_t numPlants; bool hardwareFailure; bool debug;);
